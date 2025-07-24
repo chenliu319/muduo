c74882ad6f4bd0c490473ce4023f6594e6f8d90a
chenliu319 <102792230+chenliu319@users.noreply.github.com>
2025-07-24 20:23:48 +0800

    Update zinxTimer.cc

--- zinxTimer.cc
+++ zinxTimer.cc
@@ -1,39 +1,76 @@
-while(1)
+gameProtocol.h
+class gameChannel;
+class gameProtocol : public Iprotocol
 {
-	....
-	//位操作
-	//为什么使用位操作而不使用复制或赋值，因为这里是小端字节序，低位在低位，高位在高位，有些机器默认是大端字节序，会导致低位在高位，高位在低位导致乱序
-	//数据内容要求的长度
-	//把4个字节拼成一个32位整数
-	int iLength = 0;
-	//由于szLast位是由低到高，所以szLast[0]表示最低位，左移0位（即不变），然后用按位或（|=）赋值给 iLength。第一个字节放在最低的8位（int整数的第一个字节）。
-	iLength |= szLast[0] << 0;
-	//取 szlast[1] 这个字节，左移8位（相当于乘以256），放到 iLength 的第8~15位（int整数的第二个字节）。
-	iLength |= szLast[1] << 8;
-	//取 szlast[2] 这个字节，左移16位，放到 iLength 的第16~23位（int整数的第三个字节）。
-	iLength |= szLast[2] << 16;
-	//取 szlast[3] 这个字节，左移24位，放到 iLength 的第24~31位（int整数的第四个字节）。
-	iLength |= szLast[3] << 24;
-
-	//数据id
-	int id = 0;
-	id |= szLast[4] << 0;
-	id |= szLast[5] << 8;
-	id |= szLast[5] << 16;
-	id |= szLast[7] << 24;
-
-	//通过读到的长度判断后续的报文是否合法,szLast.size() - 8:接收到的数据内容的长度
-	//如果后续报文的长度小于规定的数据内容长度，说明发生了粘包且收到的数据内容长度小于实际的数据内容要求的长度
-	if((szLast.size() - 8) < iLength)
-	{
-		//本条接收到的报文长度不够，啥都不干继续接收后续传来的报文
-		return pRet;
-	}
-	
-	//后续接收到的报文没有发生粘包或者发生了粘包但是接收到的数据内容长度大于实际的数据内容要求的长度
-	//构造一条用户请求,szLast.substr(8,iLength)表示将收到的报文从第8位开始截取，一直截取到实际的数据内容要求的长度，这就是一条不粘包的完整报文
-	gameMsg* pMsg = new gameMsg(id,szLast.substr(8,iLength));
-	//如果收到的报文长度大于实际报文长度,弹出已经处理成功的报文，剩下的就是多余的报文
-	szLast = szLast.substr(8+iLength,szLast.size() - 8 -iLength);
+...
+public:
+	gameChannel* m_channel = NULL;
 }
 
+gameProtocol.cc
+#include "gameChannel.h"
+
+gameChannel.cc
+CreateTcpDataChannel(int _fd)
+{
+...
+//绑定协议对象,双向绑定,通道层能访问到协议层，协议层能访问通道层
+pProtocol->m_channel = pChannel;
+}
+
+
+//参数来自业务层，是待发送的消息，返回值是转换后的原始数据
+response2raw(UserData& _oUserData)
+{
+	//消息内容长度
+	int iLength = 0;
+	//消息id
+	int id = 0;
+	//消息的具体内容
+	std::string msgContent;
+	
+	GET_REF2DATA(gameMsg,oOutPut,_oUserData);
+	id = oOutPut.enMsgType;
+	msgContent = oOutPut.serialize();
+	iLength = msgContent.size();
+	
+	auto pret = new std::string();
+	/*
+		为什么右移后与0xff进行&就能得到对应的字节
+		假设有一个int类型的变量 iLength，它在内存中占4个字节（32位）iLength = 0x12345678,它的二进制表示是：00010010 00110100 01010110 01111000（高位在左，低位在右）
+		右移操作 iLength >> n，会把所有位向右移动n位。例如：
+		iLength >> 0 不变，还是原始值
+		iLength >> 8 所有位右移8位，原来的第2个字节变成最低字节
+		iLength >> 16 所有位右移16位，原来的第3个字节变成最低字节
+		iLength >> 24 所有位右移24位，原来的第4个字节变成最低字节
+		
+		与0xff进行按位与（&）,0xff 的二进制是 11111111，也就是只保留最低8位，其余全部清零
+		(iLength >> n) & 0xff 的作用就是：把右移n位后的最低8位保留下来，其余全部清零。
+	*/
+	//前4字节是int型，且是从低位到高位
+	//第1个字节就是长度的最低位,取出最低8位（第1个字节）放入pret第0个位置
+	pret.push_back((iLength >> 0) & 0xff);
+	//取出第2个字节放入pret第1个位置
+	pret.push_back((iLength >> 8) & 0xff);
+	//取出第3个字节放入pret第2个位置
+	pret.push_back((iLength >> 16) & 0xff);
+	//取出最高字节（第4个字节）放入pret第3个位置
+	pret.push_back((iLength >> 24) & 0xff);
+	
+	//继续将id也拼进pret
+	pret.push_back((id >> 0) & 0xff);
+	pret.push_back((id >> 8) & 0xff);
+	pret.push_back((id >> 16) & 0xff);
+	pret.push_back((id >> 24) & 0xff);
+	
+	//将后续的消息内容拼接到pret中
+	pret->append(msgContent);
+	
+	return pret;
+}
+
+//返回数据发送的通道，也就是指定使用哪个通道来发送
+GetMsgSender(BytesMsg& _oByte)
+{
+	return m_channel;
+}
